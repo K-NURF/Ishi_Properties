9786a9f5f8a7a3c7cc1906aa3c07a37283005cbc
K_NURF <franklinkaranja747@gmail.com>
2022-10-16 18:39:54 +0300

    landing page bg not working yet

--- Ishi/resources/views/home.blade.php
+++ Ishi/resources/views/home.blade.php
@@ -2,4 +2,5 @@
     <h1 class="text-4xl">Ishi Properties</h1>
     <p>An all in one place for all your real estate and reated business needs</p>  
     <p>Buy properties and sell your properties here</p> 
+    <p>Rent apartments and Lease apartments</p>
 </x-layout>
