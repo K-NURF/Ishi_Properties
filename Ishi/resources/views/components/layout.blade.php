09c960e8c06adb73e52bc016d763c92e567b731b
judejudo <86878327+judejudo@users.noreply.github.com>
2022-10-18 21:33:00 +0300

    landing page update

--- Ishi/resources/views/components/layout.blade.php
+++ Ishi/resources/views/components/layout.blade.php
@@ -30,6 +30,7 @@
     <link rel="stylesheet" href="../css/main-footer.css">
     <link rel="stylesheet" href="css/style6.css">
     <link rel="stylesheet" href="../css/style6.css">
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body class="mb-24">
     <nav class="flex justify-between items-center mb-4">
--- Ishi/resources/views/home.blade.php
+++ Ishi/resources/views/home.blade.php
@@ -1,11 +1,47 @@
 <x-layout>
-    {{-- <div class="absolute top-28 left-0 w-full h-3/4 bg-no-repeat bg-center opacity-90"
-        style="background-image: url('images/pexels-alexander-isreb-1797393.jpg')"></div> --}}
-    <div class="z-10 ml-5">
-        <h1 class="text-5xl">Ishi Properties</h1><br><br>
-        <p>An all in one place for all your real estate and reated business needs</p>
-        <p>Buy properties and sell your properties here</p>
-        <p>Rent apartments and Lease apartments</p>
-    </div>
-    @include('partials._chatbox')
+    <section> 
+          <div class="content">
+            <h1 class="landing">Ishi Homes</h1>
+            <p class="landing">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia nostrum
+              dignissimos quam atque rerum sint facilis velit fugiat? Reiciendis
+              obcaecati ducimus, accusantium hic laboriosam aliquam! Consequatur ipsam
+              fugit rem illum.</p>
+      
+            <div class="pages">
+              <a href="/properties" class="btn">Buy/Rent</a>
+              <a href="/property/all" class="btn">Management/Sell</a>
+            </div>
+          </div>
+        </section>
+        <span class="divider"></span>
+        <div class="section2">
+          <div class="section2part1">What we <br> Ishi homes Do <br> For you</div>
+          <div class="section2part2">
+            <ul class="honeycomb">
+                <li class="honeycomb-cell">
+                    <img class="honeycomb-cell_img" src="images/management.jpg">
+                    <div class="honeycomb-cell_title">Management</div>
+                </li>
+                <li class="honeycomb-cell">
+                    <img class="honeycomb-cell_img" src="images/buy.jpg">
+                    <div class="honeycomb-cell_title">Buy</div>
+                </li>
+        
+                <li class="honeycomb-cell">
+                    <img class="honeycomb-cell_img" src="images/leasing.jpg">
+                    <div class="honeycomb-cell_title">Leasing</div>
+                </li>
+                <li class="honeycomb-cell">
+                    <img class="honeycomb-cell_img" src="images/rent.jpg  ">
+                    <div class="honeycomb-cell_title">Rental</div>
+                </li>
+                <li class="honeycomb-cell honeycomb_Hidden">
+                </li>
+            </ul>
+          </div>
+        </div>
+        <div class="section3">
+          <img class="mark"  src="images/real-estate-465100_1280.jpg">
+        </div>
+   
 </x-layout>
