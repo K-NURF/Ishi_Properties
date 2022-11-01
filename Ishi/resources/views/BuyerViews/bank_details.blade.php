93a9e5d22d15ad76563c81109dabb0d7aebb0140
Franklin <85135768+K-NURF@users.noreply.github.com>
2022-11-02 00:46:49 +0300

    bank details page

---
+++ Ishi/resources/views/BuyerViews/bank_details.blade.php
@@ -0,0 +1,16 @@
+<x-buyerLayout>
+    <x-card class="p-10 max-w-lg mx-auto mt-12 mb-12">
+        <header>
+            <h1>Bank Details</h1>
+        </header>
+
+        <p>These are the bank details</p>
+        <p>4749852541212210</p>
+        <p>4749852541212210</p>
+        <p>4749852541212210</p>
+
+        <button class="bg-blue-300 text-white rounded hover:bg-blue-600">
+            <i class="fa-solid fa-circle-check"></i>Paid
+        </button>
+    </x-card>
+</x-buyerLayout>
